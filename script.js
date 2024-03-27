daffa01de9e2555678ec0568d0d9722aa3526c3d
Zihad <huda.zihadul.23@gmail.com>
2024-03-27 10:03:25 +0600

    basic js practice begin

---
+++ script.js
@@ -0,0 +1,23 @@
+function visibility(){
+    document.getElementById('visibility').style.visibility = 'visible'
+}
+function invisibility(){
+    document.getElementById('visibility').style.visibility = 'hidden'
+}
+
+function arrowOne(){
+    document.getElementById('arrowKey').src='img/arrow-circle-right.png'
+}
+
+function arrowTwo(){
+    document.getElementById('arrowKey').src='img/arrow-circle-right_solid.png'
+}
+
+function textSwitch(){
+    document.getElementById("jstest").innerHTML = "Like This!"
+}
+
+function windowAlert(){
+   window.alert("This is a window alert.") 
+}
+
